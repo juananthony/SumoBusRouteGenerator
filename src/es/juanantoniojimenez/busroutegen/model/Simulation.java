34e95dcb3c0bf380460d26c1aa56bfea94b771ab
juananthony <juanantoniojimenez@me.com>
2014-06-21 20:40:21 +0200

    Created a new java class to manage all Sumo simulations

---
+++ src/es/juanantoniojimenez/busroutegen/model/Simulation.java
@@ -0,0 +1,9 @@
+package es.juanantoniojimenez.busroutegen.model;
+
+import java.util.HashMap;
+
+public class Simulation {
+	private HashMap<String, Edge> edges = new HashMap<String, Edge>();
+	
+	
+}
