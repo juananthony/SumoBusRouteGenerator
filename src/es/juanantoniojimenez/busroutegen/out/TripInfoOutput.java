9d6529a2b1be06909f4dd6724058a0ec9f6dbe05
juananthony <juanantoniojimenez@me.com>
2014-06-21 21:31:02 +0200

    New functionality
    Completed output file read functionality and added a new method to get
    mean speed in output.

--- src/es/juanantoniojimenez/busroutegen/out/TripInfoOutput.java
+++ src/es/juanantoniojimenez/busroutegen/out/TripInfoOutput.java
@@ -2,6 +2,7 @@ package es.juanantoniojimenez.busroutegen.out;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 
 import javax.xml.parsers.DocumentBuilder;
@@ -9,13 +10,14 @@ import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 
 import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
 import es.juanantoniojimenez.busroutegen.exception.SumoException;
 import es.juanantoniojimenez.busroutegen.model.TripInfo;
 
 public class TripInfoOutput {
-	private HashMap<String, TripInfo> hash = new HashMap<String, TripInfo>();
+	private HashMap<String, TripInfo> tripInfos = new HashMap<String, TripInfo>();
 	
 	/**
 	 * Empty constructor.
@@ -31,7 +33,7 @@ public class TripInfoOutput {
 	 * @throws SumoException Throws a SumoException if some error appears.
 	 */
 	public TripInfoOutput(String outputFile) throws SumoException {
-		File fXmlFile = new File("/Users/juananthony/Desktop/castellana/castellana.net.xml");
+		File fXmlFile = new File(outputFile);
 		DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
 		DocumentBuilder dBuilder;
 		try {
@@ -41,11 +43,34 @@ public class TripInfoOutput {
 		}
 		try {
 			Document doc = dBuilder.parse(fXmlFile);
+			doc.getDocumentElement().normalize();
+			NodeList nList = doc.getElementsByTagName("tripinfo");
+			for (int i = 0; i < nList.getLength(); i++) {
+				TripInfo tripInfo = new TripInfo(nList.item(i));
+				tripInfos.put(tripInfo.getId(), tripInfo);
+			}
 		} catch (SAXException e) {
 			throw new SumoException("ERROR: parsing xml. SAXException.");
 		} catch (IOException e) {
 			throw new SumoException("ERROR: Error loading xml file. IOException.");
 		}
+		
+	}
+	
+	/**
+	 * Returns the mean speead of simulation.
+	 * @return double with the mean speed of all trip infos in simulation.
+	 */
+	public double getMeanSpeed() {
+		double retValue = 0.0;
+		ArrayList<TripInfo> trips = new ArrayList(tripInfos.values());
+		
+		for(TripInfo trip : trips) {
+			retValue += (trip.getRouteLength()/trip.getDuration());
+		}
+		
+		retValue = retValue / trips.size();
+		return retValue;
 	}
 
 	/**
@@ -53,10 +78,14 @@ public class TripInfoOutput {
 	 * @return
 	 */
 	public HashMap<String, TripInfo> getHash() {
-		return hash;
+		return tripInfos;
 	}
 
+	/**
+	 * 
+	 * @param hash
+	 */
 	public void setHash(HashMap<String, TripInfo> hash) {
-		this.hash = hash;
+		this.tripInfos = hash;
 	}
 }
